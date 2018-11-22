f9587368bd7fc875955da53fb945c8145917eefb
dsalamancaMS <desalama@microsoft.com>
2018-11-22 11:24:25 -0600

    Added Structs

---
+++ Hello-Go/src/Examples/structs.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+	type courseMeta struct {
+		Author string
+		Level  string
+		name   string
+		Rating float64
+	}
+	/*
+	   To create a struct of type courseMeta:
+	   var DockerCourse courseMeta
+	   DockerCourse := new(courseMeta)
+	   DockerCourse := courseMeta{
+	   	Author: "Denis Salamanca",
+	   	LeveL: "Advanced",
+	   	name: "Docker Course"
+	   	Rating 5,
+	   }
+
+
+	*/
+}
